da019e88ebd6b1f01ff24e16e1c7eb30f5ea2f85
Roy Elkabetz <elkabetzroy@gmail.com>
2021-07-01 22:41:15 +0300

    Added classes to models.py and utils.py files

--- src/models.py
+++ src/models.py
@@ -3,6 +3,26 @@ import torch
 from torch import nn
 
 
+#########
+import json
+import pandas as pd
+import numpy as np
+import torch
+from pathlib import Path
+from torch.utils.data import Dataset, DataLoader
+import pytorch_lightning as pl
+from pytorch_lightning.callbacks import ModelCheckpoint
+from pytorch_lightning.loggers import TensorBoardLogger
+from sklearn.model_selection import train_test_split
+from termcolor import colored
+
+from transformers import (
+    AdamW,
+    T5ForConditionalGeneration,
+    T5TokenizerFast as T5Tokenizer
+)
+#########
+
 class TextClassificationModel(nn.Module):
     """Simple text classification model (taken from Pytorch.Tutorials)"""
     def __init__(self, vocab_size, embed_dim, num_class):
@@ -131,3 +151,80 @@ class Seq2Seq(nn.Module):
             # if not, use predicted token
             decoder_input = trg[t] if teacher_force else top1
         return outputs
+
+
+class NewsSummaryModel(pl.LightningModule):
+
+    def __init__(self, model_name):
+        super().__init__()
+        self.model = T5ForConditionalGeneration.from_pretrained(model_name, return_dict=True)
+
+    def forward(self, input_ids, attention_mask, decoder_attention_mask, labels=None):
+        output = self.model(
+            input_ids,
+            attention_mask=attention_mask,
+            labels=labels,
+            decoder_attention_mask=decoder_attention_mask
+        )
+
+        return output.loss, output.logits
+
+    def training_step(self, batch, batch_idx):
+        input_ids = batch['text_input_ids']
+        attention_mask = batch['text_attention_mask']
+        labels = batch['labels']
+        labels_attention_mask = batch['labels_attention_mask']
+
+        loss, outputs = self(
+            input_ids=input_ids,
+            attention_mask=attention_mask,
+            decoder_attention_mask=labels_attention_mask,
+            labels=labels
+        )
+        metrics = {'train_loss': loss, 'train_perp': torch.exp(loss)}
+        self.log_dict(metrics, prog_bar=True, logger=True)
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        input_ids = batch['text_input_ids']
+        attention_mask = batch['text_attention_mask']
+        labels = batch['labels']
+        labels_attention_mask = batch['labels_attention_mask']
+
+        loss, outputs = self(
+            input_ids=input_ids,
+            attention_mask=attention_mask,
+            decoder_attention_mask=labels_attention_mask,
+            labels=labels
+        )
+        metrics = {'valid_loss': loss, 'valid_perp': torch.exp(loss)}
+        self.log_dict(metrics, prog_bar=True, logger=True)
+        return loss
+
+    def test_step(self, batch, batch_idx):
+        input_ids = batch['text_input_ids']
+        attention_mask = batch['text_attention_mask']
+        labels = batch['labels']
+        labels_attention_mask = batch['labels_attention_mask']
+
+        loss, outputs = self(
+            input_ids=input_ids,
+            attention_mask=attention_mask,
+            decoder_attention_mask=labels_attention_mask,
+            labels=labels
+        )
+        metrics = {'test_loss': loss, 'test_perp': torch.exp(loss)}
+        self.log_dict(metrics, prog_bar=True, logger=True)
+        return loss
+
+    def configure_optimizers(self):
+        optimizer = AdamW(self.parameters(), lr=0.001)
+        scheduler = torch.optim.lr_scheduler.StepLR(optimizer, 2, gamma=0.1)
+        lr_dict = {'optimizer': optimizer,
+                   'lr_scheduler': {'scheduler': scheduler,
+                                    'interval': 'epoch',
+                                    'monitor': 'valid_loss',
+                                    }
+                   }
+        return lr_dict
+
--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,27 @@ from torch.utils.data import Dataset, DataLoader
 import pytorch_lightning as pl
 
 
+#########
+import json
+import pandas as pd
+import numpy as np
+import torch
+from pathlib import Path
+from torch.utils.data import Dataset, DataLoader
+import pytorch_lightning as pl
+from pytorch_lightning.callbacks import ModelCheckpoint
+from pytorch_lightning.loggers import TensorBoardLogger
+from sklearn.model_selection import train_test_split
+from termcolor import colored
+
+from transformers import (
+    AdamW,
+    T5ForConditionalGeneration,
+    T5TokenizerFast as T5Tokenizer
+)
+#########
+
+
 class DataFrameDataset(Dataset):
     """This class create a torch.utils.data.Dataset from a pandas.DataFrame or from a CSV file."""
 
@@ -138,3 +159,131 @@ class IMDBClassificationTask(pl.LightningModule):
                                     }
                    }
         return lr_dict
+
+
+class NewsSummaryDataset(Dataset):
+
+    def __init__(
+        self,
+        data: pd.DataFrame,
+        tokenizer: T5Tokenizer,
+        text_max_token_len: int = 512,
+        summary_max_token_len: int = 128
+    ):
+        self.tokenizer = tokenizer
+        self.data = data
+        self.text_max_token_len = text_max_token_len
+        self.summary_max_token_len = summary_max_token_len
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, index: int):
+        data_row = self.data.iloc[index]
+
+        text = data_row['text']
+        summary = data_row['summary']
+
+        # i added self
+        text_encoding = self.tokenizer(
+            text,
+            max_length=self.text_max_token_len,
+            padding='max_length',
+            truncation=True,
+            return_attention_mask=True,
+            add_special_tokens=True,
+            return_tensors='pt'
+        )
+
+        summary_encoding = self.tokenizer(
+            summary,
+            max_length=self.summary_max_token_len,
+            padding='max_length',
+            truncation=True,
+            return_attention_mask=True,
+            add_special_tokens=True,
+            return_tensors='pt'
+        )
+
+        labels = summary_encoding['input_ids']
+        labels[labels == 0] = -100
+
+        return dict(
+            text=text,
+            summary=summary,
+            text_input_ids=text_encoding['input_ids'].flatten(),
+            text_attention_mask=text_encoding['attention_mask'].flatten(),
+            labels=labels.flatten(),
+            labels_attention_mask=summary_encoding['attention_mask'].flatten()
+        )
+
+
+class NewsSummaryDataModule(pl.LightningDataModule):
+
+    def __init__(
+        self,
+        train_df: pd.DataFrame,
+        valid_df: pd.DataFrame,
+        test_df: pd.DataFrame,
+        tokenizer: T5Tokenizer,
+        batch_size: int = 10,
+        text_max_token_len: int = 512,
+        summary_max_token_len: int = 128
+    ):
+        super().__init__()
+
+        self.train_df = train_df
+        self.valid_df = valid_df
+        self.test_df = test_df
+        self.batch_size = batch_size
+        self.tokenizer = tokenizer
+        self.text_max_token_len = text_max_token_len
+        self.summary_max_token_len = summary_max_token_len
+
+    def setup(self, stage=None):
+        self.train_dataset = NewsSummaryDataset(
+            self.train_df,
+            self.tokenizer,
+            self.text_max_token_len,
+            self.summary_max_token_len
+        )
+
+        self.valid_dataset = NewsSummaryDataset(
+            self.valid_df,
+            self.tokenizer,
+            self.text_max_token_len,
+            self.summary_max_token_len
+        )
+
+        self.test_dataset = NewsSummaryDataset(
+            self.test_df,
+            self.tokenizer,
+            self.text_max_token_len,
+            self.summary_max_token_len
+        )
+
+    def train_dataloader(self):
+        return DataLoader(
+            self.train_dataset,
+            batch_size=self.batch_size,
+            shuffle=True,
+            num_workers=2
+        )
+
+    def val_dataloader(self):
+        return DataLoader(
+            self.valid_dataset,
+            batch_size=self.batch_size,
+            shuffle=False,
+            num_workers=2
+        )
+
+    def test_dataloader(self):
+        return DataLoader(
+            self.test_dataset,
+            batch_size=self.batch_size,
+            shuffle=False,
+            num_workers=2
+        )
+
+
